abcf787c6b6874d174ddaf5720a5ed12a85ab036
lachie-aerialrobotics <lachlan.orr15@imperial.ac.uk>
2024-03-11 19:06:14 +0100

    compatibility changes

--- src/ros_numpy/point_cloud2.py
+++ src/ros_numpy/point_cloud2.py
@@ -118,6 +118,10 @@ def pointcloud2_to_array(cloud_msg, squeeze=True):
     dtype_list = fields_to_dtype(cloud_msg.fields, cloud_msg.point_step)
 
     # parse the cloud into an array
+    print(cloud_msg.data)
+
+    print(dtype_list)
+
     cloud_arr = np.frombuffer(cloud_msg.data, dtype_list)
 
     # remove the dummy fields that were added
