06e270e97bc90476f85a7a579c7c74f2043159b9
lachie-aerialrobotics <lachlan.orr15@imperial.ac.uk>
2024-03-11 19:18:21 +0100

    compatibility changes

--- src/ros_numpy/point_cloud2.py
+++ src/ros_numpy/point_cloud2.py
@@ -117,8 +117,6 @@ def pointcloud2_to_array(cloud_msg, squeeze=True):
     # construct a numpy record type equivalent to the point type of this cloud
     dtype_list = fields_to_dtype(cloud_msg.fields, cloud_msg.point_step)
 
-    print(np.shape(cloud_msg.data))
-
     # parse the cloud into an array
     cloud_arr = np.frombuffer(cloud_msg.data, dtype_list)
 
